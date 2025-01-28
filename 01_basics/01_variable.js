601290edfd557e8b32ba4512c5c470b189d82112
Unnati7574 <unnatip323@gmail.com>
2025-01-28 11:08:42 +0000

    variable and datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId1 = "5787"
+let accountName = "unnati patel"
+var accountEmail = "unnati.patel@gmail.com"
+accountCity = "Ahmedabad"
+let accountState;
+// accountId = "123456" 
+
+console.table([accountId1, accountName, accountEmail, accountCity, accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newes version
+
+// alert(2+2) we are using node.js not browser
+
+let name01 = "unnati"
+let age = 24
+let isMarried = true
+let state;
+
+console.table([name01, age, isMarried, state])
+
+// number => 2 power 53
+// bigint => big number
+// string => '' ""
+// boolean => true/false
+// null =>  //standalon value
+// undefine => not empty but value not define
+
+// symbol => unique value
+// object =>  
+
+console.log(typeof age);
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
