2e789eb52bfbe3a1aef527c2c54ebd0652fcfaa2
Unnati7574 <unnatip323@gmail.com>
2025-01-30 07:56:50 +0000

    conversion operator

---
+++ 01_basics/03_conversionoperators.js
@@ -0,0 +1,88 @@
+
+/* conversion operators => like variable are
+                                             number, string, boolean ?  */
+
+
+
+
+                                             let score= "33abf"  // number is NaN => not a number
+
+                                             console.log(typeof score);
+                                             
+                                             console.log(typeof (score));
+                                             
+                                             let valueIsNumber = Number(score)                   //Number =>  any number, null, undefined
+                                             console.log(valueIsNumber); 
+                                             console.log(typeof valueIsNumber);
+                                             
+                                             
+                                             
+                                             let score1= null  // zero 
+                                             
+                                             // console.log(typeof score1);
+                                             
+                                             // console.log(typeof (score1));
+                                             
+                                             let valueAreNumber = Number(score1) 
+                                             // console.log(typeof valueAreNumber);
+                                             // console.log(valueAreNumber); 
+                                             
+                                             
+                                             let score2= undefined                       // let score = true -> value is 1
+                                             
+                                             // console.log(typeof score2);
+                                             
+                                             // console.log(typeof (score2));
+                                             
+                                             let valueIsNumbers = Number(score2)
+                                             // console.log(typeof valueIsNumbers);
+                                             // console.log(valueIsNumbers); 
+                                             
+                                             
+                                             // 33 => 33
+                                             // "33abs" => NaN
+                                             // null => 0
+                                             // undefined => NaN
+                                             // true => 1; false =>0
+                                             
+                                             
+                                             let logInnumber = 1                   // Define a function to demonstrate conversion operators
+                                             
+                                             
+                                             let logInnumberboolean = Boolean(logInnumber)                 // boolean => true or false if 1 or 0
+                                             
+                                             console.log( logInnumberboolean);
+                                             
+                                             console.log(typeof logInnumberboolean);   
+                                             
+                                             
+                                             // 1 => true 0 => false    // if we conver this
+                                             //  "" => flase
+                                             // "unnati" => true
+                                             
+                                             
+                                             let numberisString = 1    // Define a function to demonstrate conversion operators
+                                             
+                                             let numberisStringboolean = Boolean(numberisString)                 // boolean => true or false if 1
+                                             
+                                             console.log(numberisStringboolean);
+                                             console.log(typeof numberisStringboolean);
+                                             
+                                             
+                                             let someString = 33
+                                             let someStringIs = String(someString)
+                                             console.log(someStringIs);
+                                             console.log(typeof someStringIs);
+                                             
+                                             
+                                             
+                                             
+                                             //  ex   object array
+                                             
+                                             
+                                             
+                                             
+                                             
+                                             
+                                             
+                                             
