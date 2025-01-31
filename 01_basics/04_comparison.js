14592ad4e4dc90ef52951c464146a83dc9cc3ab8
Unnati7574 <unnatip323@gmail.com>
2025-01-31 08:30:17 +0000

    comparison operator

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,44 @@
+// // console.log(2 < 4);
+// // console.log(2 <= 5);
+// // console.log(4 > 8);
+// // console.log(6 >= 9);
+// // console.log(8 != 6);
+
+
+// console.log("2" > 10);
+// console.log("02" > 1);
+
+                    // avoid null and udefined type comparison
+
+// in javascript null is considered as 0 in comparison with numbers
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   //here null is 0 but greater an equal to 0 means true
+
+
+console.log(undefined == 0);  // always output are false.
+console.log(undefined < 0);
+
+
+console.log(1 == 1);
+console.log(2 == 4);
+
+
+// string check or strict ckeck
+// ===   // when we use this operator it checks both value and data type
+
+
+console.log(2 === "2");
+console.log(2 === 2); 
+
+
+
+
+
+
+
+
+
+
+
