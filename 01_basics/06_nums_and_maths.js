dcb243c37f334f75fbc614c20003e71f72ca6f34
Unnati7574 <unnatip323@gmail.com>
2025-02-03 10:59:41 +0000

    NUMS AND MATHS

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+const score = 500
+console.log(score);
+
+const balance = new Number(1200)
+console.log(balance);
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2))  // used in app like 100.00
+
+const othernumber = 456.8789
+console.log(othernumber.toPrecision(3));  //precision on number
+
+const hundred = 100000
+console.log(hundred.toLocaleString('en-IN'));
+
